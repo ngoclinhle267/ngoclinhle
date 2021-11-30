1134fa5c27c0d1e523ba4c88559c8020f0d1a574
ngoclinhle267 <85571848+ngoclinhle267@users.noreply.github.com>
2021-11-30 16:58:30 +0700

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,15 @@
+$('.bxslider').bxSlider({
+mode: 'horizontal',
+infiniteLoop: true,
+auto: true,
+autoStart: true,
+autoDirection: 'next',
+autoHover: true,
+pause: 3000,
+autoControls: false,
+pager: true,
+pagerType: 'full',
+controls: true,
+captions: true,
+speed: 500
+});
